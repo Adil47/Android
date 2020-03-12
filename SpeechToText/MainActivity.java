b02a4e52b9a517db8ef5a7666096a4c85d38b149
Muhammad Adil Kamal <Name.AK47@gmail.com>
2020-03-12 12:35:41 +0500

    remove some line of import packges

--- SpeechToText/MainActivity.java
+++ SpeechToText/MainActivity.java
@@ -1,18 +1,4 @@
-package com.example.myapplication;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.speech.RecognizerIntent;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import java.util.ArrayList;
-import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
